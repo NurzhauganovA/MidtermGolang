5032a88a69630b1dff8910ce4c5089c0b7456f2b
NurzhauganovA <90419990+NurzhauganovA@users.noreply.github.com>
2023-02-26 23:17:13 +0600

    Added Middleware!

--- package/service/auth.go
+++ package/service/auth.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"crypto/sha1"
+	"errors"
 	"fmt"
 	"github.com/NurzhauganovA/online-store/endpoint"
 	"github.com/NurzhauganovA/online-store/package/repository"
@@ -12,7 +13,6 @@ import (
 const (
 	salt       = "ahajasdfwerq23123423442mmsdf"
 	signingKey = "qwerjdfigjdf34534kfksdlfasdfxczv"
-	tokenTTL   = 12 * time.Hour
 )
 
 type tokenClaims struct {
@@ -48,6 +48,26 @@ func (s *AuthService) GenerateToken(username, password string) (string, error) {
 	})
 
 	return token.SignedString([]byte(signingKey))
+}
+
+func (s *AuthService) ParseToken(accessToken string) (int, error) {
+	token, err := jwt.ParseWithClaims(accessToken, &tokenClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("invalid signing method")
+		}
+
+		return []byte(signingKey), nil
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	claims, ok := token.Claims.(*tokenClaims)
+	if !ok {
+		return 0, errors.New("token claims are not of type *tokenClaims")
+	}
+
+	return claims.UserId, nil
 }
 
 func generatePasswordHash(password string) string {
--- package/service/service.go
+++ package/service/service.go
@@ -8,6 +8,7 @@ import (
 type Authorization interface {
 	CreateUser(user endpoint.User) (int, error)
 	GenerateToken(username, password string) (string, error)
+	ParseToken(token string) (int, error)
 }
 
 type Category interface {
